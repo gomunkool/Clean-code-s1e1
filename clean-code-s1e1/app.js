178933160b0e5bc9cf51e04a22b2964717240d7a
gomunkool <75207234+gomunkool@users.noreply.github.com>
2021-11-25 22:57:02 +0300

    fix: 2.1 BAM

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -9,7 +9,7 @@
 
 var taskInput = document.getElementById('new-task') //Add a new task.
 var addButton = document.getElementsByTagName('button')[0] //first button
-var incompleteTaskHolder = document.getElementById('incompleteTasks') //ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById('incomplete-tasks') //ul of #incompleteTasks
 var completedTasksHolder = document.getElementById('completed-tasks') //completed-tasks
 
 //New task list item
@@ -58,7 +58,6 @@ var addTask = function () {
   //Create a new list item with the text from the #new-task:
   if (!taskInput.value) return
   var listItem = createNewTaskElement(taskInput.value)
-
   //Append listItem to incompleteTaskHolder
   incompleteTaskHolder.appendChild(listItem)
   bindTaskEvents(listItem, taskCompleted)
